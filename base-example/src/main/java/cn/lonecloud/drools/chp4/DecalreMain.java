d5529ae05e498de0a3a1c79b964abab6beed76bf
lonecloud <lonecloudmac@icloud.com>
2020-02-22 16:53:04 +0800

    :sparkles: declare 语法糖

---
+++ base-example/src/main/java/cn/lonecloud/drools/chp4/DecalreMain.java
@@ -0,0 +1,24 @@
+package cn.lonecloud.drools.chp4;
+
+import cn.lonecloud.drools.bean.Person;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * @author lonecloud
+ * @version v1.0
+ * @Package cn.lonecloud.drools.chp4
+ * @Description: TODO
+ * @date 2020/2/2211:14 AM
+ */
+public class DecalreMain {
+    public static void main(String[] args) {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieClasspathContainer = kieServices.getKieClasspathContainer();
+        KieSession globalSession = kieClasspathContainer.newKieSession("decl");
+        int i = globalSession.fireAllRules();
+        System.out.println("一共运行了"+i+"条规则");
+        globalSession.dispose();
+    }
+}
