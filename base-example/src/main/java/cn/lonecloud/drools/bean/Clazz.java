cf4bddd5424ac3de262f9fb7e441e470ae2bee48
lonecloud <lonecloudmac@icloud.com>
2020-02-23 21:08:09 +0800

    :sparkles: from 语法糖

---
+++ base-example/src/main/java/cn/lonecloud/drools/bean/Clazz.java
@@ -0,0 +1,26 @@
+package cn.lonecloud.drools.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author lonecloud
+ * @version v1.0
+ * @Package cn.lonecloud.drools.bean
+ * @Description: TODO
+ * @date 2020/2/238:31 PM
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Clazz {
+
+    private String grade;
+
+    private String number;
+
+
+}
